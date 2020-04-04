29d0a84ca888f948b69814f674d0a7186d822bc4
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-04 22:42:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,9 @@
 def unsafe?(speed)
-if speed > 60 || speed < 40
+if speed < 40 || speed > 60
  true
-else speed < 60 && speed > 40
+elsif speed < 60 && speed > 40
   false
-else speed > 40 || speed < 60
-  false
+
 end
 
 
